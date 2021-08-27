f91b2dfede35004215859bf435a0aa1e91219e2a
jalibutod <aalibutod@pic-informatica.it>
2021-08-27 17:50:39 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ciao Vivi! <3
+          Ciao Vivi! Mwah~
         </p>
         <a
           className="App-link"
